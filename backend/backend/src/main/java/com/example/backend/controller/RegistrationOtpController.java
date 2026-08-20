d944ea03c51268dba0fa4b72f2a383ddd09673a6
“Aaryan-Nandan” <aarya.nandan25@gmail.com>
2026-08-21 00:00:14 +0530

    add

--- backend/backend/src/main/java/com/example/backend/controller/RegistrationOtpController.java
+++ backend/backend/src/main/java/com/example/backend/controller/RegistrationOtpController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/customer")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class RegistrationOtpController {
 
     @Autowired
