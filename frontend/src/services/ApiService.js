39683292e7322044a0ac0bcbe2efe1c910308eec
“Aaryan-Nandan” <aarya.nandan25@gmail.com>
2026-08-22 06:18:38 +0530

    Switch OTP email to Gmail SMTP

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -1,20 +1,11 @@
-// import axios from "axios";
-// import API_URL from "../config";
-
-// const BASE_URL = API_URL;
-
-// export const getAllVariants = async () => {
-//     return await axios.get(`${BASE_URL}/variant/all`);
-// };
 import api from "./axiosConfig";
 
 // ============================================================
 // GET ALL CAR VARIANTS
 // ============================================================
 
-//export const getAllVariants = async () => {
-   // const response = await api.get("/variant/all");
-   // return response.data;
-   export const getAllVariants = async () => {
-    return await api.get("/variant/all");
+export const getAllVariants = async () => {
+    const response = await api.get("/variant/all");
+
+    return response.data;
 };
